acfa6e2208e1367bed02ed43e192aa4081067737
Rennn <trungnghia294@gmail.com>
2024-11-20 18:43:43 +0700

    addConnectorPostgres

--- spark/apps/batch/gasstation/common/connection/__init__.py
+++ spark/apps/batch/gasstation/common/connection/__init__.py
@@ -1,4 +1,6 @@
 from .redis_connector import get_redis_client
 from .lakefs_connector import get_lakefs_client, get_lakefs
+from .postgres_connector import get_postgres_properties
 
-__all__ = ['get_redis_client', 'get_lakefs_client', 'get_lakefs']
+__all__ = ['get_redis_client', 'get_lakefs_client',
+           'get_lakefs', 'get_postgres_properties']
