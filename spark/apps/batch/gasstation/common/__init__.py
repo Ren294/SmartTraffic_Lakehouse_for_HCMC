2baf2a9fa4c738c5a19133e00a386bc2f9f3be78
Rennn <trungnghia294@gmail.com>
2024-11-20 18:44:04 +0700

    modifyModule

--- spark/apps/batch/gasstation/common/__init__.py
+++ spark/apps/batch/gasstation/common/__init__.py
@@ -1,5 +1,5 @@
 from .config import create_spark_session
-from .connection import get_lakefs_client, get_lakefs, get_redis_client
+from .connection import get_lakefs_client, get_lakefs, get_redis_client, get_postgres_properties
 
 __all__ = ['create_spark_session', 'get_lakefs_client',
-           'get_lakefs', 'get_redis_client']
+           'get_lakefs', 'get_redis_client', 'get_postgres_properties']
