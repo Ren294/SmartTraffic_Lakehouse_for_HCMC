23f1511a5bea1c0c9f8ab523d2f0767f87fefe7a
Rennn <trungnghia294@gmail.com>
2024-11-22 23:22:47 +0700

    additionPySparkForStagingGasstation

---
+++ spark/apps/batch/gasstation/SilverCheckChangesStaging_inventorytransaction.py
@@ -0,0 +1,4 @@
+from SilverStagingTableProcessor import ChangeProcessor
+if __name__ == "__main__":
+    processor = ChangeProcessor("inventorytransaction")
+    processor.check_changes()
---
+++ spark/apps/batch/gasstation/SilverCheckChangesStaging_product.py
@@ -0,0 +1,4 @@
+from SilverStagingTableProcessor import ChangeProcessor
+if __name__ == "__main__":
+    processor = ChangeProcessor("product")
+    processor.check_changes()
