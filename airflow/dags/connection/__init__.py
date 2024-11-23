552e173dfdf531175b16b2e7201b8b2842d048a0
Rennn <trungnghia294@gmail.com>
2024-11-23 22:38:43 +0700

    updateSparkSubmitModule

--- airflow/dags/connection/__init__.py
+++ airflow/dags/connection/__init__.py
@@ -1,4 +1,5 @@
 from .redis_connector import get_redis_client
 from .lakefs_connector import get_lakefs_client, get_lakefs
-
-__all__ = ['get_redis_client', 'get_lakefs_client', 'get_lakefs']
+from .spark_submit import spark_submit
+__all__ = ['get_redis_client', 'get_lakefs_client',
+           'get_lakefs', 'spark_submit']
---
+++ airflow/dags/connection/spark_submit.py
@@ -0,0 +1,2 @@
+def spark_submit(file_path):
+    return f"spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.3,org.apache.kafka:kafka-clients:3.2.0,org.apache.hudi:hudi-spark3.5-bundle_2.12:0.15.0,org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk-bundle:1.11.1026,io.lakefs:hadoop-lakefs-assembly:0.2.4,org.postgresql:postgresql:42.7.4,org.apache.spark:spark-avro_2.12:3.5.3 /opt/spark-apps/{file_path}"
