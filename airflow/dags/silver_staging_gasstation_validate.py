856342b3cf09c51ad3ce8027c17f9a98c4e45610
Rennn <trungnghia294@gmail.com>
2024-11-23 14:47:04 +0700

    fixBugForDAG

--- airflow/dags/silver_staging_gasstation_validate.py
+++ airflow/dags/silver_staging_gasstation_validate.py
@@ -34,6 +34,24 @@ def create_modified_branch(**context):
     return branch_name
 
 
+def push_branch_to_redis(table_name, **context):
+    """Push branch name to Redis with table-specific key"""
+    modified_branch = context['task_instance'].xcom_pull(
+        task_ids='create_modified_branch', key='modified_branch')
+
+    redis_client = get_redis_client()
+    redis_key = f"modified_branch_gasstation_{table_name}"
+
+    # Clear existing key if any
+    redis_client.delete(redis_key)
+
+    # Push new branch name
+    redis_client.rpush(redis_key, modified_branch)
+    print(f"Pushed branch {modified_branch} to Redis key {redis_key}")
+
+    return {'redis_key': redis_key, 'branch': modified_branch}
+
+
 def push_to_redis_queue(table_name, **context):
     """Push table information to Redis queue for processing"""
     modified_branch = context['task_instance'].xcom_pull(
@@ -50,13 +68,13 @@ def push_to_redis_queue(table_name, **context):
     return config
 
 
-def commit_changes(table_name, **context):
+def commit_changes(**context):
     """Commit changes to staging branch after successful update"""
     client = get_lakefs_client()
     repo = Repository("silver", client=client)
     staging_branch = repo.branch("staging_gasstation")
 
-    commit_message = f"Updated {table_name} table with latest changes"
+    commit_message = f"Updated table with latest changes"
     commit = staging_branch.commit(message=commit_message)
     return {'status': 'success', 'commit_id': commit.get_commit().id}
 
@@ -91,6 +109,7 @@ tables = ['customer', 'employee', 'gasstation', 'inventorytransaction',
           'invoice', 'invoicedetail', 'product', 'storagetank']
 
 # Create tasks for each table
+push_branch_tasks = {}
 check_tasks = {}
 push_queue_tasks = {}
 update_tasks = {}
@@ -98,13 +117,18 @@ commit_tasks = {}
 
 for table in tables:
     # Check changes task
+    push_branch_tasks[table] = PythonOperator(
+        task_id=f'push_branch_redis_{table}',
+        python_callable=push_branch_to_redis,
+        op_kwargs={'table_name': table},
+        provide_context=True,
+        dag=dag
+    )
     check_tasks[table] = SSHOperator(
         task_id=f'check_changes_{table}',
         ssh_hook=ssh_hook,
         command=f"/opt/spark/bin/spark-submit --master spark://spark-master:7077 "
-        f"--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0,"
-        f"org.apache.hudi:hudi-spark3.2-bundle_2.12:0.15.0,"
-        f"org.postgresql:postgresql:42.2.18 "
+        f"--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0,org.apache.kafka:kafka-clients:3.2.0,org.apache.hudi:hudi-spark3.2-bundle_2.12:0.15.0,org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk-bundle:1.11.1026,io.lakefs:hadoop-lakefs-assembly:0.2.4,org.postgresql:postgresql:42.7.4 "
         f"/opt/spark-apps/batch/gasstation/SilverCheckChangesStaging_{
             table}.py",
         dag=dag
@@ -124,26 +148,32 @@ for table in tables:
         task_id=f'update_hudi_{table}',
         ssh_hook=ssh_hook,
         command=f"/opt/spark/bin/spark-submit --master spark://spark-master:7077 "
-        f"--packages org.apache.hudi:hudi-spark3.2-bundle_2.12:0.15.0 "
+        f"--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0,org.apache.kafka:kafka-clients:3.2.0,org.apache.hudi:hudi-spark3.2-bundle_2.12:0.15.0,org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk-bundle:1.11.1026,io.lakefs:hadoop-lakefs-assembly:0.2.4,org.postgresql:postgresql:42.7.4 "
         f"/opt/spark-apps/batch/gasstation/SilverUpdateStaging_{table}.py",
         dag=dag
     )
 
-    # Commit changes task
-    commit_tasks[table] = PythonOperator(
-        task_id=f'commit_changes_{table}',
-        python_callable=commit_changes,
-        op_kwargs={'table_name': table},
-        provide_context=True,
-        dag=dag
-    )
+    # # Commit changes task
+    # commit_tasks[table] = PythonOperator(
+    #     task_id=f'commit_changes_{table}',
+    #     python_callable=commit_changes,
+    #     op_kwargs={'table_name': table},
+    #     provide_context=True,
+    #     dag=dag
+    # )
 
+commit_task = PythonOperator(
+    task_id='commit_changes',
+    python_callable=commit_changes,
+    provide_context=True,
+    dag=dag
+)
 # End task
 end_dag = DummyOperator(task_id='end_dag', dag=dag)
 
 # Set up dependencies
 start_dag >> create_branch_task
-
+commit_task >> end_dag
 for table in tables:
-    create_branch_task >> check_tasks[table] >> push_queue_tasks[table] >> \
-        update_tasks[table] >> commit_tasks[table] >> end_dag
+    create_branch_task >> push_branch_tasks[table] >> check_tasks[table] >> \
+        push_queue_tasks[table] >> update_tasks[table] >> commit_task
