45fdb8c92522c9a8978b33f9a9d6370a9545cd95
Rennn <trungnghia294@gmail.com>
2024-11-23 14:48:11 +0700

    renameFileJobSpark

--- spark/apps/batch/gasstation/SilverUpdateStaging_product.py
+++ spark/apps/batch/gasstation/SilverUpdateStaging_product.py
@@ -1,4 +1,4 @@
 from SilverStagingTableProcessor import ChangeProcessor
 if __name__ == "__main__":
-    processor = ChangeProcessor("invoicedetail")
+    processor = ChangeProcessor("product")
     processor.update_hudi()
