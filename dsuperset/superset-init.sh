d10884a7641f44208a7548cfd607f6b315bdc179
Rennn <trungnghia294@gmail.com>
2024-12-02 13:24:10 +0700

    addAuthorInfoForDockerFile

--- dsuperset/superset-init.sh
+++ dsuperset/superset-init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# create Admin user, you can read these values from env or anywhere else possible
+################################################################################################
+# Project: SmartTraffic_Lakehouse_for_HCMC
+# Author: Nguyen Trung Nghia (ren294)
+# Contact: trungnghia294@gmail.com
+# GitHub: Ren294
+################################################################################################
+
 superset fab create-admin --username "$ADMIN_USERNAME" --firstname Superset --lastname Admin --email "$ADMIN_EMAIL" --password "$ADMIN_PASSWORD"
 
-# Upgrading Superset metastore
 superset db upgrade
-
-# setup roles and permissions
 superset superset init
-
-# Starting server
 /bin/sh -c /usr/bin/run-server.sh
