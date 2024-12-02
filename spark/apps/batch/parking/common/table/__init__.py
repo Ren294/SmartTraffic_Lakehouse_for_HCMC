5ae6f8da9cb2695c14a033dbbae14b9dcf781da8
Rennn <trungnghia294@gmail.com>
2024-12-02 13:26:21 +0700

    addAuthorInforForConfigParking

--- spark/apps/batch/parking/common/table/__init__.py
+++ spark/apps/batch/parking/common/table/__init__.py
@@ -1,3 +1,9 @@
+"""
+  Project: SmartTraffic_Lakehouse_for_HCMC
+  Author: Nguyen Trung Nghia (ren294)
+  Contact: trungnghia294@gmail.com
+  GitHub: Ren294
+"""
 from .table_config import TableConfig
 
 __all__ = ['TableConfig']
