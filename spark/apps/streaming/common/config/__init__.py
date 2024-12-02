1f33402e32ffddda45eb492d7a2ddcff96df716e
Rennn <trungnghia294@gmail.com>
2024-12-02 13:27:06 +0700

    addAuthorInforForConfigSparkStreaming

--- spark/apps/streaming/common/config/__init__.py
+++ spark/apps/streaming/common/config/__init__.py
@@ -1,3 +1,10 @@
+
+"""
+  Project: SmartTraffic_Lakehouse_for_HCMC
+  Author: Nguyen Trung Nghia (ren294)
+  Contact: trungnghia294@gmail.com
+  GitHub: Ren294
+"""
 from .spark_config import create_spark_session
 
 __all__ = ['create_spark_session']
