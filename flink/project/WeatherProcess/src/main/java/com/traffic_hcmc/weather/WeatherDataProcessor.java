7d378c44ff33e448acc67b15289ed37eab693cb9
Rennn <trungnghia294@gmail.com>
2024-12-02 13:39:57 +0700

    addAuthorInforForWeatherProcessFlink

--- flink/project/WeatherProcess/src/main/java/com/traffic_hcmc/weather/WeatherDataProcessor.java
+++ flink/project/WeatherProcess/src/main/java/com/traffic_hcmc/weather/WeatherDataProcessor.java
@@ -1,3 +1,9 @@
+//////////////////////////////////////////////////////////////////////////////
+// Project: SmartTraffic_Lakehouse_for_HCMC
+// Author: Nguyen Trung Nghia (ren294)
+// Contact: trungnghia294@gmail.com
+// GitHub: Ren294
+//////////////////////////////////////////////////////////////////////////////
 package com.traffic_hcmc.weather;
 
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
