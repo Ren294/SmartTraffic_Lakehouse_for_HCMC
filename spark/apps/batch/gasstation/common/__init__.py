00907aae885d3e4d2bd59c3f460487b5a4c1a486
Rennn <trungnghia294@gmail.com>
2024-12-02 13:25:59 +0700

    addAuthorInforForConfigGasstation

--- spark/apps/batch/gasstation/common/__init__.py
+++ spark/apps/batch/gasstation/common/__init__.py
@@ -1,3 +1,9 @@
+"""
+  Project: SmartTraffic_Lakehouse_for_HCMC
+  Author: Nguyen Trung Nghia (ren294)
+  Contact: trungnghia294@gmail.com
+  GitHub: Ren294
+"""
 from .config import create_spark_session
 from .connection import get_lakefs_client, get_lakefs, get_redis_client, get_postgres_properties
 from .table import TableConfig
--- spark/apps/batch/gasstation/common/connection/postgres_connector.py
+++ spark/apps/batch/gasstation/common/connection/postgres_connector.py
@@ -1,3 +1,11 @@
+"""
+  Project: SmartTraffic_Lakehouse_for_HCMC
+  Author: Nguyen Trung Nghia (ren294)
+  Contact: trungnghia294@gmail.com
+  GitHub: Ren294
+"""
+
+
 def get_postgres_properties():
     return {
         "driver": "org.postgresql.Driver",
