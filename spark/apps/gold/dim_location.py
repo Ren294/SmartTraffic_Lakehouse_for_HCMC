182b26b60aaeb63a8121b10edd40b7fa347dc4e1
Rennn <trungnghia294@gmail.com>
2024-12-08 00:16:27 +0700

    fixJoinBug

--- spark/apps/gold/dim_location.py
+++ spark/apps/gold/dim_location.py
@@ -11,66 +11,83 @@ from common import read_silver_main, create_spark_session, get_lakefs, write_to_
 
 
 def create_dim_location(spark, path):
-    gasstation_df = read_silver_main(spark, "gasstation/gasstation").select(
-        col("gasstationid").alias("station_id"),
-        col("address").alias("station_address")
+    gasstation_df = read_silver_main(spark, "gasstation/gasstation") \
+        .select(
+            split(col("address"), ", ").getItem(0).alias("Street"),
+            split(col("address"), ", ").getItem(1).alias("District"),
+            split(col("address"), ", ").getItem(2).alias("City"),
+            lit(None).alias("Latitude"),
+            lit(None).alias("Longitude"),
+            lit('00700').alias("PostalCode"),
+            lit("Vietnam").alias("Country"),
+            lit("Ho Chi Minh").alias("Region"),
+            lit(None).alias("Geospatial_Coordinates")
     )
 
-    parking_df = read_silver_main(spark, "parking/parkinglot").select(
-        col("parkinglotid").alias("parking_id"),
-        col("address").alias("parking_address")
+    parking_location_df = read_silver_main(spark, "parking/parkinglot") \
+        .select(
+            split(col("location"), ", ").getItem(0).alias("Street"),
+            split(col("location"), ", ").getItem(1).alias("District"),
+            split(col("location"), ", ").getItem(2).alias("City"),
+            lit(None).alias("Latitude"),
+            lit(None).alias("Longitude"),
+            lit('00700').alias("PostalCode"),
+            lit("Vietnam").alias("Country"),
+            lit("Ho Chi Minh").alias("Region"),
+            lit(None).alias("Geospatial_Coordinates")
     )
 
-    accident_df = read_silver_main(spark, "accident").select(
-        col("accidentid").alias("accident_id"),
-        col("street").alias("accident_street"),
-        col("district").alias("accident_district"),
-        col("city").alias("accident_city"),
-        col("latitude").alias("accident_latitude"),
-        col("longitude").alias("accident_longitude"),
-        col("postalcode").alias("accident_postalcode")
+    accident_location_df = read_silver_main(spark, "accidents") \
+        .select(
+            col("road_name").alias("Street"),
+            col("district").alias("District"),
+            col("city").alias("City"),
+            lit(None).alias("Latitude"),
+            lit(None).alias("Longitude"),
+            lit(None).alias("PostalCode"),
+            lit("Vietnam").alias("Country"),
+            lit("Ho Chi Minh").alias("Region"),
+            lit(None).alias("Geospatial_Coordinates")
     )
 
-    weather_df = read_silver_main(spark, "weather").select(
-        col("stationid").alias("weather_station_id"),
-        col("street").alias("weather_street"),
-        col("district").alias("weather_district"),
-        col("city").alias("weather_city"),
-        col("latitude").alias("weather_latitude"),
-        col("longitude").alias("weather_longitude")
+    weather_location_df = read_silver_main(spark, "weather") \
+        .select(
+            col("address").alias("Street"),
+            lit(None).alias("District"),
+            lit("Ho Chi Minh").alias("City"),
+            lit(None).alias("Latitude"),
+            lit(None).alias("Longitude"),
+            lit('00700').alias("PostalCode"),
+            lit("Vietnam").alias("Country"),
+            lit("Ho Chi Minh").alias("Region"),
+            concat_ws(", ", col("latitude"), col("longitude")
+                      ).alias("Geospatial_Coordinates")
     )
 
-    traffic_df = read_silver_main(spark, "traffic").select(
-        col("trafficid").alias("traffic_id"),
-        col("street").alias("traffic_street"),
-        col("district").alias("traffic_district"),
-        col("city").alias("traffic_city"),
-        col("latitude").alias("traffic_latitude"),
-        col("longitude").alias("traffic_longitude")
+    traffic_location_df = read_silver_main(spark, "traffic") \
+        .select(
+            col("street").alias("Street"),
+            col("district").alias("District"),
+            col("city").alias("City"),
+            col("latitude").alias("Latitude"),
+            col("longitude").alias("Longitude"),
+            lit('00700').alias("PostalCode"),
+            lit("Vietnam").alias("Country"),
+            lit("Ho Chi Minh").alias("Region"),
+            concat_ws(", ", col("latitude"), col("longitude")
+                      ).alias("Geospatial_Coordinates")
     )
 
-    combined_df = gasstation_df \
-        .unionByName(parking_df, allowMissingColumns=True) \
-        .unionByName(accident_df, allowMissingColumns=True) \
-        .unionByName(weather_df, allowMissingColumns=True) \
-        .unionByName(traffic_df, allowMissingColumns=True)
+    combined_df = gasstation_df.unionByName(parking_location_df, allowMissingColumns=True) \
+        .unionByName(accident_location_df, allowMissingColumns=True) \
+        .unionByName(weather_location_df, allowMissingColumns=True) \
+        .unionByName(traffic_location_df, allowMissingColumns=True)
 
-    dim_location_df = combined_df.select(
-        monotonically_increasing_id().alias("LocationKey"),
-        col("station_address").alias("Street"),
-        col("accident_district").alias("District"),
-        col("accident_city").alias("City"),
-        col("accident_latitude").alias("Latitude"),
-        col("accident_longitude").alias("Longitude"),
-        col("accident_postalcode").alias("PostalCode"),
-        lit("Vietnam").alias("Country"),
-        lit("Asia").alias("Region"),
-        concat_ws(", ", col("Latitude"), col("Longitude")
-                  ).alias("Geospatial_Coordinates")
-    )
+    final_dim_location = combined_df.withColumn(
+        "LocationKey", monotonically_increasing_id())
 
-    write_to_warehouse(dim_location_df, "dim_location", path,
-                       recordkey="LocationKey", precombine="LocationKey")
+    write_to_warehouse(final_dim_location, "dim_location",
+                       path, recordkey="LocationKey", precombine="LocationKey")
 
 
 if __name__ == "__main__":
