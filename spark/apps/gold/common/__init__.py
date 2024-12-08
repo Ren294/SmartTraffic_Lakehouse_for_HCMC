70bbcc0a29bc1c6ba5552dfddfd527421cdb1be0
Rennn <trungnghia294@gmail.com>
2024-12-09 00:00:41 +0700

    addConfigModule

--- spark/apps/gold/common/__init__.py
+++ spark/apps/gold/common/__init__.py
@@ -7,6 +7,6 @@
 from .config import create_spark_session
 from .connection import get_lakefs_client, get_lakefs, get_redis_client, get_postgres_properties
 from .table import TableConfig
-from .operator import write_to_warehouse, create_table_warehouse, read_silver_main
+from .operator import write_to_warehouse, create_table_warehouse, read_silver_main, read_warehouse
 __all__ = ['create_spark_session', 'get_lakefs_client',
-           'get_lakefs', 'get_redis_client', 'get_postgres_properties', 'TableConfig', 'write_to_warehouse', 'create_table_warehouse', 'read_silver_main']
+           'get_lakefs', 'get_redis_client', 'get_postgres_properties', 'TableConfig', 'write_to_warehouse', 'create_table_warehouse', 'read_silver_main', 'read_warehouse']
--- spark/apps/gold/common/operator/__init__.py
+++ spark/apps/gold/common/operator/__init__.py
@@ -1,4 +1,5 @@
 from .hive_operate import create_table_warehouse
-from .hudi_operator import read_silver_main, write_to_warehouse
+from .hudi_operator import read_silver_main, write_to_warehouse, read_warehouse
 
-__all__ = ['create_table_warehouse', 'read_silver_main', 'write_to_warehouse']
+__all__ = ['create_table_warehouse', 'read_silver_main',
+           'write_to_warehouse', 'read_warehouse']
